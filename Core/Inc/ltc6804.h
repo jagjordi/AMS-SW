4c4db94d5374b5e36fb7460b6800b03f5115946b
Jordi <jordiag@kth.se>
2025-07-30 11:42:07 +0200

    first commit

---
+++ Core/Inc/ltc6804.h
@@ -0,0 +1,13 @@
+/*
+ * ltc6804.h
+ *
+ *  Created on: Jul 30, 2025
+ *      Author: Jordi
+ */
+
+#ifndef INC_LTC6804_H_
+#define INC_LTC6804_H_
+
+
+
+#endif /* INC_LTC6804_H_ */
---
+++ Core/Src/ltc6804.c
@@ -0,0 +1,8 @@
+/*
+ * ltc6804.c
+ *
+ *  Created on: Jul 30, 2025
+ *      Author: Jordi
+ */
+
+
